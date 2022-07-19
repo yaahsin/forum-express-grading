35f9bf230c0f426c723561fa4132da289e253ed4
yaahsin <brillanteoboe@gmail.com>
2022-07-19 17:18:55 +0800

    feat: add api-auth middleware

--- routes/apis/index.js
+++ routes/apis/index.js
@@ -3,12 +3,13 @@ const router = express.Router()
 const passport = require('../../config/passport')
 const admin = require('./modules/admin')
 const restController = require('../../controllers/apis/restaurant-controller')
+const { authenticated, authenticatedAdmin } = require('../../middleware/api-auth')
 const { apiErrorHandler } = require('../../middleware/error-handler')
 const userController = require('../../controllers/apis/user-controller')
 
-router.use('/admin', admin)
+router.use('/admin', authenticated, authenticatedAdmin, admin)
 
-router.get('/restaurants', restController.getRestaurants)
+router.get('/restaurants', authenticated, restController.getRestaurants)
 router.use('/', apiErrorHandler) // 每個經過"/"的路由都要經過這handler
 router.post('/signin', passport.authenticate('local', { session: false }), userController.signIn)
 
