60d604b2b9f79e6df64175220640d290d2d2bbba
yaahsin <brillanteoboe@gmail.com>
2022-07-19 16:48:18 +0800

    feat: api signin add jwt token

---
+++ controllers/apis/user-controller.js
@@ -0,0 +1,21 @@
+const jwt = require('jsonwebtoken')
+const userController = {
+  signIn: (req, res, next) => {
+    try {
+      //  jwt.sign(payload, secretOrPrivateKey, [options, callback])
+      const userData = req.user.toJSON()
+      delete userData.password
+      const token = jwt.sign(userData, process.env.JWT_SECRET, { expiresIn: '30d' }) // 簽發 JWT，效期為 30 天
+      res.json({
+        status: 'success',
+        data: {
+          token,
+          user: req.user
+        }
+      })
+    } catch (err) {
+      next(err)
+    }
+  }
+}
+module.exports = userController
--- routes/apis/index.js
+++ routes/apis/index.js
@@ -1,12 +1,15 @@
 const express = require('express')
 const router = express.Router()
+const passport = require('../../config/passport')
 const admin = require('./modules/admin')
 const restController = require('../../controllers/apis/restaurant-controller')
 const { apiErrorHandler } = require('../../middleware/error-handler')
+const userController = require('../../controllers/apis/user-controller')
 
 router.use('/admin', admin)
 
 router.get('/restaurants', restController.getRestaurants)
 router.use('/', apiErrorHandler) // 每個經過"/"的路由都要經過這handler
+router.post('/signin', passport.authenticate('local', { session: false }), userController.signIn)
 
 module.exports = router
