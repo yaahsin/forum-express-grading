f6466a7c4b6a08dd9e10ccb2391ee5fb55aacf00
yaahsin <brillanteoboe@gmail.com>
2022-08-22 11:46:53 +0800

    feat: add getRestaurants api

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ const session = require('express-session')
 const passport = require('./config/passport')
 const handlebarsHelpers = require('./helpers/handlebars-helpers')
 const { getUser } = require('./helpers/auth-helpers')
-const { pages } = require('./routes')
+const { pages, apis } = require('./routes')
 
 const app = express()
 const port = process.env.PORT || 3000
@@ -32,6 +32,8 @@ app.use((req, res, next) => {
   res.locals.user = getUser(req)
   next()
 })
+
+app.use('/api', apis)
 app.use(pages)
 
 app.listen(port, () => {
---
+++ routes/apis/index.js
@@ -0,0 +1,6 @@
+const express = require('express')
+const router = express.Router()
+
+const restController = require('../../controllers/apis/restaurant-controller')
+router.get('/restaurants', restController.getRestaurants)
+module.exports = router
